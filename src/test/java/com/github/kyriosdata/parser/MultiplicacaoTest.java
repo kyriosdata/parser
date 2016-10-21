2edbc88023ddb46cbb64e414ab26eaa0295fa3aa
Fábio Nogueira de Lucena <fabio@engenhariadesoftware.inf.br>
2016-10-20 22:01:43 -0200

    bug fix

--- src/test/java/com/github/kyriosdata/parser/MultiplicacaoTest.java
+++ src/test/java/com/github/kyriosdata/parser/MultiplicacaoTest.java
@@ -25,7 +25,7 @@ public class MultiplicacaoTest {
     @Test
     public void multiplicacoesDeConstantesSemContexto() {
         assertEquals(2f, constantes(1f, 2f).valor(), 0.1d);
-        assertEquals(21f, constantes(1f, -20f).valor(), 0.1d);
+        assertEquals(-20f, constantes(1f, -20f).valor(), 0.1d);
     }
 
     @Test
