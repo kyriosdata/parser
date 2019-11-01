a742bb783c7f51fba514e69019a8b698efc5b2c4
kyriosdata <fabio@inf.ufg.br>
2019-11-01 15:42:18 -0300

    versao com método estatico recendo argumentos

--- src/test/java/com/github/kyriosdata/parser/GeradorTest.java
+++ src/test/java/com/github/kyriosdata/parser/GeradorTest.java
@@ -11,14 +11,7 @@ import java.lang.reflect.Method;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 
-import static org.objectweb.asm.Opcodes.ACC_PUBLIC;
-import static org.objectweb.asm.Opcodes.ACC_STATIC;
-import static org.objectweb.asm.Opcodes.GETSTATIC;
-import static org.objectweb.asm.Opcodes.IADD;
-import static org.objectweb.asm.Opcodes.INVOKEVIRTUAL;
-import static org.objectweb.asm.Opcodes.LDC;
-import static org.objectweb.asm.Opcodes.RETURN;
-import static org.objectweb.asm.Opcodes.V11;
+import static org.objectweb.asm.Opcodes.*;
 
 class GeradorTest {
 
@@ -72,11 +65,56 @@ class GeradorTest {
         System.out.println(clazz.getName());
 
     }
+
+    @Test
+    void b() throws IOException, NoSuchMethodException,
+            InvocationTargetException, IllegalAccessException {
+        ClassWriter cw = new ClassWriter(0);
+        cw.visit(V11, ACC_PUBLIC, "B", null, "java/lang/Object", null);
+
+        final int PUBLIC_STATIC = ACC_PUBLIC + ACC_STATIC;
+        MethodVisitor main = cw.visitMethod(PUBLIC_STATIC, "calcule",
+                "(II)I", null, null);
+        main.visitCode();
+
+        main.visitCode();
+        main.visitVarInsn(ILOAD, 0);
+        main.visitVarInsn(ILOAD, 1);
+        main.visitInsn(IADD);
+        main.visitInsn(IRETURN);
+        main.visitMaxs(2, 2);
+        main.visitEnd();
+        cw.visitEnd();
+
+        byte[] bytecodes = cw.toByteArray();
+
+        Path dirCorrente = FileSystems.getDefault().getPath(".");
+        final Path path = Path.of(dirCorrente.toString(), "B.class");
+
+        try (FileOutputStream outputStream =
+                     new FileOutputStream(path.toString())) {
+            outputStream.write(bytecodes);
+        }
+
+        // Carregando e executando a classe criada dinamicamente
+        DynamicClassLoader loader = new DynamicClassLoader();
+        Class<?> clazz = loader.defineClass("B", bytecodes);
+        Class<?>[] tipos = new Class[] { Integer.TYPE, Integer.TYPE };
+        Method method = clazz.getDeclaredMethod("calcule", tipos);
+        Object[] params = { 1, 7 };
+        Object retorno = method.invoke(null, params);
+        System.out.println(clazz.getName());
+        System.out.println(retorno);
+    }
 }
 
 
 class DynamicClassLoader extends ClassLoader {
     public Class<?> defineClass(String name, byte[] b) {
         return defineClass(name, b, 0, b.length);
+    }
+
+    public static int calcule(int x, int y) {
+        return x + y;
     }
 }
