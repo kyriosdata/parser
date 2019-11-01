18c6faba9578aa2fa73aa80129771e0c58143f83
kyriosdata <fabio@inf.ufg.br>
2019-11-01 15:05:37 -0300

    gerador chama metodo criado dinamicamente

--- src/test/java/com/github/kyriosdata/parser/GeradorTest.java
+++ src/test/java/com/github/kyriosdata/parser/GeradorTest.java
@@ -6,6 +6,8 @@ import org.objectweb.asm.MethodVisitor;
 
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 
@@ -21,28 +23,32 @@ import static org.objectweb.asm.Opcodes.V11;
 class GeradorTest {
 
     @Test
-    void a() throws IOException {
+    void a() throws IOException, NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         ClassWriter cw = new ClassWriter(0);
         cw.visit(V11, ACC_PUBLIC, "A", null, "java/lang/Object", null);
-        MethodVisitor main = cw.visitMethod(ACC_PUBLIC + ACC_STATIC, "main",
+
+        final int PUBLIC_STATIC = ACC_PUBLIC + ACC_STATIC;
+        MethodVisitor main = cw.visitMethod(PUBLIC_STATIC, "main",
                 "([Ljava/lang/String;)V", null, null);
         main.visitCode();
-        main.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io/PrintStream;");
+        main.visitFieldInsn(GETSTATIC, "java/lang/System", "out", "Ljava/io" +
+                "/PrintStream;");
 
         // Coloca no topo da pilha o valor 1
-        final int c1 = cw.newConst(1);
+        final int c1 = cw.newConst(10);
         main.visitInsn(LDC);
         main.visitInsn(c1);
 
         // Coloca no topo da pilha o valor 5
-        final int c2 = cw.newConst(5);
+        final int c2 = cw.newConst(51);
         main.visitInsn(LDC);
         main.visitInsn(c2);
 
         // Adiciona elementos no topo da pilha
         main.visitInsn(IADD);
 
-        main.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println", "(I)V", false);
+        main.visitMethodInsn(INVOKEVIRTUAL, "java/io/PrintStream", "println",
+                "(I)V", false);
         main.visitInsn(RETURN);
         main.visitMaxs(5, 2);
         main.visitEnd();
@@ -57,5 +63,20 @@ class GeradorTest {
                      new FileOutputStream(path.toString())) {
             outputStream.write(bytecodes);
         }
+
+        // Carregando e executando a classe criada dinamicamente
+        DynamicClassLoader loader = new DynamicClassLoader();
+        Class<?> clazz = loader.defineClass("A", bytecodes);
+        Method method = clazz.getMethod("main", String[].class);
+        method.invoke(null, (Object) new String[0]);
+        System.out.println(clazz.getName());
+
+    }
+}
+
+
+class DynamicClassLoader extends ClassLoader {
+    public Class<?> defineClass(String name, byte[] b) {
+        return defineClass(name, b, 0, b.length);
     }
 }
